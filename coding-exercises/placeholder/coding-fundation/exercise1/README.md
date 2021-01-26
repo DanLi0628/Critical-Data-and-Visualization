f8c925b9c4a8136dd8a3b6e1352a5bd54f7f4d47
Linda <dl4224@nyu.edu>
2021-01-26 20:31:34 +0800

    final version

--- coding-exercises/placeholder/coding-fundation/exercise1/README.md
+++ coding-exercises/placeholder/coding-fundation/exercise1/README.md
@@ -1,3 +1,3 @@
 Difficulties that I encounter:
 (1) unfamiliar with getElementsByClassName and appendChild,
-(2) don't know how to create certain number of rectangles because of (1)
+(2) don't know how to create certain number of rectangles because of (1).
--- coding-exercises/placeholder/coding-fundation/exercise1/script.js
+++ coding-exercises/placeholder/coding-fundation/exercise1/script.js
@@ -3,7 +3,7 @@ document.getElementById('btn').addEventListener('click',createSquares);
 
 function createSquares(){
   //get the number
-  var n = document.getElementById('number').value;
+  var n = document.getElementById('txt').value;
   document.getElementsByClassName('container')[0].innerHTML='';
 
   //draw squares
