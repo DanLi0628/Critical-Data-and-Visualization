15917d48365d292cea53259c528e4d13bc863446
Linda <dl4224@nyu.edu>
2021-01-26 19:37:23 +0800

    improve

--- coding-exercises/placeholder/coding-fundation/exercise1/script.js
+++ coding-exercises/placeholder/coding-fundation/exercise1/script.js
@@ -4,15 +4,12 @@ document.getElementById('btn').addEventListener('click',createSquares);
 function createSquares(){
   //get the number
   var n = document.getElementById('number').value;
-  console.log(n);
   document.getElementsByClassName('container')[0].innerHTML='';
-  var txt = document.getElementById('number').value;
 
   //draw squares
-  for (i = 1; i<=n; i++){
-    console.log(2);
-    let div=document.createElement('div');
-    div.className='rect';
-    document.getElementsByClassName('container')[0].appendChild(div);
+  for (var i = 1; i<=n; i++){
+    var rect=document.createElement('div');
+    rect.className='rect';
+    document.getElementsByClassName('container')[0].appendChild(rect);
   }
 }
