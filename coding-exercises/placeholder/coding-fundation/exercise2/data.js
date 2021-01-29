3709e6c7747c9cd5636e9dea6a9baa5cf3423e08
Linda <dl4224@nyu.edu>
2021-01-29 21:16:04 +0800

    first try

---
+++ coding-exercises/placeholder/coding-fundation/exercise2/data.js
@@ -0,0 +1,182 @@
+let data = [
+    {
+        "timestamp": "2021-01-27T11:52:58.316Z",
+        "emailAddress": "tz1422@nyu.edu",
+        "score": 0,
+        "tenet": 4,
+        "frozen": 4,
+        "soul": 5,
+        "avengers": 3,
+        "idiots": 4
+    },
+    {
+        "timestamp": "2021-01-27T11:53:32.095Z",
+        "emailAddress": "jz4059@nyu.edu",
+        "score": 0,
+        "tenet": 4,
+        "frozen": 3,
+        "soul": 5,
+        "avengers": 5,
+        "idiots": 3
+    },
+    {
+        "timestamp": "2021-01-27T12:00:17.606Z",
+        "emailAddress": "dl4224@nyu.edu",
+        "score": 0,
+        "tenet": 4,
+        "frozen": 3,
+        "soul": 4,
+        "avengers": 5,
+        "idiots": 4
+    },
+    {
+        "timestamp": "2021-01-27T12:01:36.699Z",
+        "emailAddress": "rx523@nyu.edu",
+        "score": 0,
+        "tenet": 5,
+        "frozen": 2,
+        "soul": 2,
+        "avengers": 3,
+        "idiots": 3
+    },
+    {
+        "timestamp": "2021-01-27T12:07:52.460Z",
+        "emailAddress": "yz6216@nyu.edu",
+        "score": 0,
+        "tenet": 4,
+        "frozen": 4,
+        "soul": 5,
+        "avengers": 3,
+        "idiots": 4
+    },
+    {
+        "timestamp": "2021-01-27T12:08:37.448Z",
+        "emailAddress": "wj716@nyu.edu",
+        "score": 0,
+        "tenet": 4,
+        "frozen": 5,
+        "soul": 3,
+        "avengers": 3,
+        "idiots": 4
+    },
+    {
+        "timestamp": "2021-01-27T12:11:26.291Z",
+        "emailAddress": "jw6785@nyu.edu",
+        "score": 0,
+        "tenet": 1,
+        "frozen": 2,
+        "soul": 1,
+        "avengers": 2,
+        "idiots": 3
+    },
+    {
+        "timestamp": "2021-01-27T17:39:46.884Z",
+        "emailAddress": "jf4043@nyu.edu",
+        "score": 0,
+        "tenet": 4,
+        "frozen": 4,
+        "soul": 5,
+        "avengers": 3,
+        "idiots": 4
+    },
+    {
+        "timestamp": "2021-01-28T03:02:16.333Z",
+        "emailAddress": "dl4224@nyu.edu",
+        "score": 0,
+        "tenet": 3,
+        "frozen": 3,
+        "soul": 3,
+        "avengers": 3,
+        "idiots": 4
+    },
+    {
+        "timestamp": "2021-01-28T03:02:27.318Z",
+        "emailAddress": "dl4224@nyu.edu",
+        "score": 0,
+        "tenet": 3,
+        "frozen": 3,
+        "soul": 4,
+        "avengers": 4,
+        "idiots": 4
+    },
+    {
+        "timestamp": "2021-01-28T03:15:22.958Z",
+        "emailAddress": "dl4224@nyu.edu",
+        "score": 0,
+        "tenet": 3,
+        "frozen": 3,
+        "soul": 4,
+        "avengers": 3,
+        "idiots": 4
+    },
+    {
+        "timestamp": "2021-01-28T03:15:41.177Z",
+        "emailAddress": "dl4224@nyu.edu",
+        "score": 0,
+        "tenet": 3,
+        "frozen": 4,
+        "soul": 3,
+        "avengers": 4,
+        "idiots": 3
+    },
+    {
+        "timestamp": "2021-01-28T07:02:05.323Z",
+        "emailAddress": "yx1750@nyu.edu",
+        "score": 0,
+        "tenet": 3,
+        "frozen": 3,
+        "soul": 3,
+        "avengers": 2,
+        "idiots": 4
+    },
+    {
+        "timestamp": "2021-01-28T07:34:32.191Z",
+        "emailAddress": "lz1802@nyu.edu",
+        "score": 0,
+        "tenet": 3,
+        "frozen": 4,
+        "soul": 4,
+        "avengers": 5,
+        "idiots": 5
+    },
+    {
+        "timestamp": "2021-01-28T09:42:47.934Z",
+        "emailAddress": "cl4690@nyu.edu",
+        "score": 0,
+        "tenet": 5,
+        "frozen": 5,
+        "soul": 3,
+        "avengers": 3,
+        "idiots": 3
+    },
+    {
+        "timestamp": "2021-01-28T10:22:01.190Z",
+        "emailAddress": "sl6899@nyu.edu",
+        "score": 0,
+        "tenet": 3,
+        "frozen": 4,
+        "soul": 5,
+        "avengers": 3,
+        "idiots": 3
+    },
+    {
+        "timestamp": "2021-01-28T11:14:33.644Z",
+        "emailAddress": "qp274@nyu.edu",
+        "score": 0,
+        "tenet": 3,
+        "frozen": 2,
+        "soul": 1,
+        "avengers": 1,
+        "idiots": 1
+    },
+    {
+        "timestamp": "2021-01-29T03:35:49.095Z",
+        "emailAddress": "lc4087@nyu.edu",
+        "score": 0,
+        "tenet": 4,
+        "frozen": 2,
+        "soul": 3,
+        "avengers": 3,
+        "idiots": 4
+    }
+]
