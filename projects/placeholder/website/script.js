3116ab017bc5759d36f327796e3ffeb22cb0ec85
Linda <dl4224@nyu.edu>
2021-02-09 23:20:20 +0800

    first draft final version

--- projects/placeholder/website/script.js
+++ projects/placeholder/website/script.js
@@ -7,19 +7,17 @@ d3.json("data.json").then(gotData);
 
 let x = [];
 let cost = [];
+let time = [];
 
 function gotData(incomingData){
+  console.log(incomingData);
   for (let i = 0; i < incomingData.length; i++){
     cost.push(incomingData[i].howMuchIsIt);
     x.push(i+1);
+    time.push(incomingData[i].whenDidYouOrderIt-900)
   }
 
-  viz.selectAll("div").data(x).enter()
-    .append("text")
-    .text(txt)
-      .attr("x", txtLocation)
-      .attr("y",250)
-
+// cost title
   viz.append("text")
       .text(title)
         .attr("id","txt")
@@ -27,21 +25,16 @@ function gotData(incomingData){
         .attr("y",50)
         .attr("font-weight",5)
   ;
-
-  viz.selectAll("div").data(x).enter()
-    .append("text")
-    .text(txt)
-      .attr("x", txtLocation)
-      .attr("y",250)
-
+// order time title
   viz.append("text")
-      .text(title)
+      .text(orderTime)
         .attr("id","txt")
-        .attr("x",550)
-        .attr("y",50)
+        .attr("x",500)
+        .attr("y",350)
         .attr("font-weight",5)
   ;
 
+//circles
   viz.selectAll("circle").data(x).enter()
     .append("circle")
       .attr("cx",xLocation)
@@ -50,7 +43,17 @@ function gotData(incomingData){
       .attr("r",money)
       .attr("fill","white")
   ;
-
+// time squares
+  viz.selectAll("rect").data(time).enter()
+    .append("rect")
+      .attr("x",timeLocation)
+      .attr("y",400)
+      .attr("width",10)
+      .attr("height",10)
+      .attr("fill","LightYellow")
+      .attr("stroke","grey")
+    ;
+//order 1234
   viz.selectAll("div").data(x).enter()
     .append("text")
     .text(txt)
@@ -58,25 +61,33 @@ function gotData(incomingData){
       .attr("y",250)
   ;
       }
-
+//time square xlocation
+function timeLocation(time){
+  return time;
+}
+//cost title
 function title(){
   return "Cost";
 }
-
+//order time title
+function orderTime(){
+  return "Order Time";
+}
+//order 1234
 function txt(x){
   return "Order " + x
 }
-
+//order 1234 xlocation
 function txtLocation(x){
   var n = x*145;
   return n;
 }
-
+//circle xlocation
 function xLocation(x){
   var n = x*150;
   return n;
 }
-
+//circle radius
 function money(cost){
   return cost * 2;
 }
