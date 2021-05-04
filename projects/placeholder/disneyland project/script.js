6d76f7e7e7a4d9a0da0ab5c017defdb6cb86debb
Linda <dl4224@nyu.edu>
2021-05-04 23:14:25 +0800

    setting

--- projects/placeholder/disneyland project/script.js
+++ projects/placeholder/disneyland project/script.js
@@ -1,12 +1,20 @@
-w = 1000;
-h = 600;
+w = window.innerWidth;
+h = window.innerHeight;
 
 let viz = d3.select("#container")
   .append("svg")
     .attr("id","viz")
     .attr("width", w)
     .attr("height", h)
-    .style("background-color", "lavender")
+    //.style("background-color", "lavender")
+;
+
+viz.append("svg:image")
+  .attr("xlink:href","disneyland daily attendance cover.png")
+  .attr("x",0)
+  .attr("y",0)
+  .attr("width",w-50)
+  .attr("height",h-50)
 ;
 
 d3.json("data.json").then(gotData);
@@ -18,18 +26,16 @@ function gotData(incomingData){
         .attr("class","datagroup")
     ;
 
+
   // group1.append("circle")
-  //   .attr("cx",0)
-  //   .attr("cy",0)
+  //   .attr("cx",function (d,i){
+  //     return i + Math.random()*w;
+  //   })
+  //   .attr("cy",function (d,i){
+  //     return i + Math.random()*h;
+  //   })
   //   .attr("r",10)
   //   .attr("fill","white")
   // ;
 
-  group1.append("image")
-    .attr("xlink:href","1.jpg")
-    .attr("x",0)
-    .attr("y",0)
-    .attr("width",800)
-    .attr("height",400)
-  ;
 }
--- projects/placeholder/disneyland project/script2.js
+++ projects/placeholder/disneyland project/script2.js
@@ -1,35 +1,70 @@
-w = 1000;
-h = 600;
+w = window.innerWidth;
+h = window.innerHeight;
+
 
 let viz = d3.select("#container")
   .append("svg")
     .attr("id","viz")
     .attr("width", w)
     .attr("height", h)
-    .style("background-color", "lavender")
+    //.style("background-color", "lavender")
 ;
+
+viz.append("svg:image")
+  .attr("xlink:href","map.jpg")
+  .attr("x",0)
+  .attr("y",0)
+  .attr("width",w-50)
+  .attr("height",h-50)
+;
+
 
 d3.json("data.json").then(gotData);
 
 function gotData(incomingData){
   console.log(incomingData);
-  let group1 = viz.selectAll(".datagroup").data(incomingData).enter()
+
+  let dwarfs = [], xDwarf = 970, yDwarf = 230;
+  let racer = [];
+  let tron = [];
+  let pirates = [];
+  let rapids = [];
+  let horizon = [];
+  let temperature = [];
+  let weather = [];
+  let festival = [];
+  let day = [];
+
+  console.log(xDwarf);
+
+
+  for (let i = 0; i < incomingData.length; i++){
+    dwarfs.push(incomingData[i].sevenDwarfsMineTrain);
+    racer.push(incomingData[i].rexsRacer);
+    tron.push(incomingData[i].tronLightcyclePowerRunPresentedByChevrolet);
+    pirates.push(incomingData[i].piratesOfTheCaribbeanBattleForTheSunkenTreasure);
+    rapids.push(incomingData[i].roaringRapids);
+    horizon.push(incomingData[i].soaringOverTheHorizon);
+    temperature.push(incomingData[i].temperature);
+    day.push(incomingData[i].whichDay);
+  }
+
+  let group1 = viz.selectAll(".dwarf").data(dwarfs).enter()
       .append("g")
-        .attr("class","datagroup")
+        .attr("class","dwarf")
     ;
+    console.log(dwarfs);
 
-  // group1.append("circle")
-  //   .attr("cx",0)
-  //   .attr("cy",0)
-  //   .attr("r",10)
-  //   .attr("fill","white")
-  // ;
-
-  group1.append("image")
-    .attr("xlink:href","2.jpg")
-    .attr("x",0)
-    .attr("y",0)
-    .attr("width",800)
-    .attr("height",400)
+  group1.append("circle")
+    .attr("cx",function (d,i){
+      if (d != "closed"){
+        return d;
+      }else{
+        return 0
+      }
+    })
+    .attr("cy",100)
+    .attr("r",3)
+    .attr("fill","black")
   ;
 }
