80f850497266a3e6e56b5a3c54179c7a3f11c297
Linda <dl4224@nyu.edu>
2021-05-10 01:47:21 +0800

    1

---
+++ projects/placeholder/disneyland project/script3.js
@@ -0,0 +1,27 @@
+w = 1200;
+h = 680;
+
+var svg = d3.select("div#container")
+  .append("svg")
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", "0 0 "+w+" "+h)
+  .classed("svg-content", true)
+;
+
+svg.append("svg:image")
+  .attr("xlink:href","back.png")
+  .attr("x",0)
+  .attr("y",0)
+  .attr("width",w)
+;
+
+d3.json("data.json").then(gotData);
+
+function gotData(incomingData){
+  console.log(incomingData);
+  let group1 = viz.selectAll(".datagroup").data(incomingData).enter()
+      .append("g")
+        .attr("class","datagroup")
+    ;
+
+}
